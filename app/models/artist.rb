15370d8e21c455ee5a96e420dc90e7a1e7ab37f0
Joanna Cortes <jococopo@gmail.com>
2023-02-20 14:00:59 -0700

    Add images model and update test

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,6 @@
 class Artist < ApplicationRecord
   belongs_to :director
+  has_many :images, dependent: :destroy
   validates :username, uniqueness: true, presence: true
 
   # may add ability for artist to log into their own account
