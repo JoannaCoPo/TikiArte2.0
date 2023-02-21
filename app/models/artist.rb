1024e2b45984d4061723a8b8af189c3e1e6f35b3
Joanna Cortes <jococopo@gmail.com>
2023-02-21 16:12:36 -0700

    Add enum for image privacy preference

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,8 +2,4 @@ class Artist < ApplicationRecord
   belongs_to :director
   has_many :images, dependent: :destroy
   validates :username, uniqueness: true, presence: true
-
-  # may add ability for artist to log into their own account
-  # validates :password, presence: true
-  # has_secure_password
 end
