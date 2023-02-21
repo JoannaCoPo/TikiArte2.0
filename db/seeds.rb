208091163aa04a8ca1ed737dea96fae1c34c08ea
Joanna Cortes <jococopo@gmail.com>
2023-02-21 16:02:10 -0700

    Add enum for image privacy preference

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,25 @@ parent_as_director = Director.create!(
   date_of_birth: Date.parse("03/04/1954")
 )
 
-parent_as_director.artists.create!(
-  username: "child_as_artist",
-  password_digest: "fridakahlo"
+child_1_as_artist = parent_as_director.artists.create!(
+  username: "child_1_as_artist",
+  password_digest: "littlebitAlexis"
 )
 
-parent_as_director.artists.create!(
+child_2_as_artist = parent_as_director.artists.create!(
   username: "child_2_as_artist",
-  password_digest: "botero"
+  password_digest: "ewDavid"
+)
+
+child_1_as_artist.images.create!(
+  title: "Sample Art 1",
+  description: "Test Image 1",
+  tags: nil
+)
+
+child_2_as_artist.images.create!(
+  title: "Sample Art 2",
+  description: "Test Image 2",
+  tags: nil,
+  privacy_status: 2
 )
