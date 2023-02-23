31f4669266f0403c63ee3921ea98ef176e84c9c2
Joanna Cortes <jococopo@gmail.com>
2023-02-23 00:55:52 -0700

    Create director with auth params

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,14 +1,29 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    binding.pry
-    # director = Director.find_or_create_by(email: params[:director][:email])
-    # director.update(director_params)
+    director = Director.find_or_create_by(email: director_params[:email])
+    director.update(first_name: director_params[:first_name], 
+                    last_name: director_params[:first_name], 
+                    token: director_params[:token], 
+                    google_id: director_params[:google_id])
     # render json: DirectorSerializer.new(director)
+    # TODO look at gem options for serializers 
   end
 
-  private
-
+  def authorization_hash
+    request.env['omniauth.auth']
+  end
+  
   def director_params
-    params.require(:director).permit(:email, :token, :google_id)
-  end
+    {
+      first_name: authorization_hash[:first_name], 
+      last_name: authorization_hash[:first_name], 
+      email: authorization_hash[:email], 
+      token: authorization_hash[:token], 
+      google_id: authorization_hash[:google_id]
+    }
+  end 
 end
+
+# define session id => current_user
+# think about non-oauth login option 
+# RegistrationService(whatever params)
